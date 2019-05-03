ad5a632c69017c4ddfabffee76f2ea88e5a36458
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-05-03 22:42:16 +0530

    Update driver method

--- core/fileDriver/fileDriver.go
+++ core/fileDriver/fileDriver.go
@@ -2,12 +2,21 @@ package fileDriver
 
 import (
 	"vision/core/fileHandler"
-	"vision/core/models"
+	"vision/core/models",
+	"errors"
 )
 
 func fileDriver(request models.QueryHolder) (string, error) {
 	isClean, err := request.Sanitise()
 	if err != nil {
 		return "", err
+	}
+
+	filePath := request.Path
+
+	if request.readFrom == "head" {
+		return fileHandler.ReadFromHead(filePath, request.Regex, request.NegateRegex, request.Limit)
+	} else {
+		return fileHandler.ReadFromTail(filePath, request.Regex, request.NegateRegex, request.Limit)
 	}
 }
