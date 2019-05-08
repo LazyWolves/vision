a5abd09ca604806fb2d4301515078e1c1f17a09d
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-05-08 22:16:13 +0530

    Fix typo

--- core/util/checkAcls.go
+++ core/util/checkAcls.go
@@ -11,7 +11,7 @@ func checkAcls(path String, configJson *models.configModel) (error) {
 		if len(blockFor) == 0 {
 			return nil
 		}
-		for _, blockedEntity := range configJson {
+		for _, blockedEntity := range configJson.BlockFor {
 			if strings.HasPrefix(path, blockedEntity) {
 				return errors.New("FILE_NOT_ALLOWED_TO_BE_VIEWED")
 			}
@@ -24,12 +24,12 @@ func checkAcls(path String, configJson *models.configModel) (error) {
 			return errors.New("FILE_NOT_ALLOWED_TO_BE_VIEWED")
 		}
 
-		for _, allowedEntity := range configJson {
+		for _, allowedEntity := range configJson.AllowFor {
 			if strings.HasPrefix(path, allowedEntity) {
 				return nil
 			}
 		}
-		
+
 		return errors.New("FILE_NOT_ALLOWED_TO_BE_VIEWED")
 	}
 }
