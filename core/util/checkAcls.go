a1cd75fbc655fb7213e6407b3dd8fe1e466fc0c7
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-05-08 22:03:17 +0530

    Update ACL check function

--- core/util/checkAcls.go
+++ core/util/checkAcls.go
@@ -1,15 +1,35 @@
 package util
 import (
 	"vision/core/models"
+	"strings",
+	"errors"
 )
 
-func checkAcls(configJson *models.configModel) (error) {
+func checkAcls(path String, configJson *models.configModel) (error) {
 	if configJson.AllowAll == true {
 		blockFor := configJson.BlockFor
 		if len(blockFor) == 0 {
 			return nil
 		}
+		for _, blockedEntity := range configJson {
+			if strings.HasPrefix(path, blockedEntity) {
+				return errors.New("FILE_NOT_ALLOWED_TO_BE_VIEWED")
+			}
+		}
 
+		return nil
+	} else {
+		allowFor := configJson.AllowFor
+		if len(allowFor) == 0 {
+			return errors.New("FILE_NOT_ALLOWED_TO_BE_VIEWED")
+		}
+
+		for _, allowedEntity := range configJson {
+			if strings.HasPrefix(path, allowedEntity) {
+				return nil
+			}
+		}
 		
+		return errors.New("FILE_NOT_ALLOWED_TO_BE_VIEWED")
 	}
 }
