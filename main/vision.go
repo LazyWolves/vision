720aead85a4ac5fc57ae0a8305b7667cdd038173
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-05-17 00:48:24 +0530

    Add comments to main package

--- main/vision.go
+++ main/vision.go
@@ -1,9 +1,12 @@
+// This is the main package which contains the entry point to Vision
 package main
 
+// Import the vision API which serves requests
 import (
 	"vision/api"
 )
 
+// Main function which starts the API process
 func main() {
 	api.Api()
 }
