8d8694bd28679b3f6b821a128aaff8c9b1f3fc0a
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-05-18 14:19:15 +0530

    Add Doc

--- core/fileHandler/head.go
+++ core/fileHandler/head.go
@@ -1,15 +1,21 @@
+// Package containing method to process resources
 package fileHandler
 
 import (
 	"os"
-	//"errors"
 	"bufio"
 	"io"
-	//"fmt"
 	"strings"
 	"vision/core/util"
 )
 
+// This function will read a given resource starting from head uptil a given numer
+// of lines as specified in limit
+// Params:
+//		path : Path to the resource on filesystem
+//		posRegex : The regex to filter lines
+//		negregex : The regex to exclude lines
+//		numLines : the number of lines to limit to
 func ReadFromHead(path, posRegex, negRegex  string, numLines int64) (string, error) {
 	fileHandle, err := os.Open(path)
 	if err != nil {
@@ -30,6 +36,8 @@ func ReadFromHead(path, posRegex, negRegex  string, numLines int64) (string, err
 		if err != nil {
 			return "", err
 		}
+
+		// Use CheckPattern function to apply the provided regexes
 		if util.CheckPattern(line, posRegex, negRegex) {
 			linesList = append(linesList, line)
 		}
