1892dc6e436d7a335da476071f7f2b93c3477445
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-05-18 14:07:44 +0530

    Add doc

--- core/fileDriver/fileDriver.go
+++ core/fileDriver/fileDriver.go
@@ -1,3 +1,4 @@
+// This package generates response for a iven request
 package fileDriver
 
 import (
@@ -6,7 +7,11 @@ import (
 	"vision/core/util"
 )
 
+// This function processes the requests and generates the response. It uses sanitise function
+// to sanitise the requests and then uses fileHandler package to generate response.
 func FileDriver(request *models.QueryHolder, aliases map[string]string, configJson *models.ConfigModel) (string, error) {
+
+	// Sanitise the request
 	isClean, err := request.Sanitise(aliases)
 	if err != nil || !isClean {
 		return "", err
@@ -14,17 +19,22 @@ func FileDriver(request *models.QueryHolder, aliases map[string]string, configJs
 
 	filePath := ""
 
+	// Set resource path, path param is given more preference than alias param.
+	// So if both path and alias are there in URL params, path is chosen.
 	if request.Path != "" {
 		filePath = request.Path
 	} else if request.Alias != "" {
 		filePath = aliases[request.Alias]
 	}
 
+	// Evaluate Acls and check it the current resource is allowed to be viewed.
+	// If allowed then proceed, if not then send suitable message back to user
 	errAcl := util.CheckAcls(filePath, configJson)
 	if errAcl != nil {
 		return "", errAcl
 	}
 
+	// Read from head or from tail as the request may be
 	if request.ReadFrom == "head" {
 		return fileHandler.ReadFromHead(filePath, request.Regex, request.NegateRegex, request.Limit)
 	} else {
