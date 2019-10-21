a6d7793de57230d651000ab827d872b123bfb070
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-10-21 16:01:38 +0530

    Add test for positive filter

--- core/fileHandler/head_test.go
+++ core/fileHandler/head_test.go
@@ -3,6 +3,7 @@ package fileHandler
 import (
 	"testing"
 	"os"
+	"strings"
 )
 
 func createTestFile() {
@@ -43,6 +44,55 @@ func TestHead(t *testing.T) {
 	// setup test file
 	createTestFile()
 
+	content, err := ReadFromHead("./test-file.txt", "", "", 5, nil)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	lines := strings.Split(content, "\n")
+	numLines := len(lines) - 1
+
+	if numLines != 5 {
+		t.Errorf("Number of lines = %d, Expected 5", numLines)
+	}
+
+	content, err = ReadFromHead("./test-file.txt", "", "", 100, nil)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	lines = strings.Split(content, "\n")
+	numLines = len(lines) - 1
+
+	if numLines != 17 {
+		t.Errorf("Number of lines = %d, Expected 17", numLines)
+	}
+
 	// remove test file
+	removeTestFile()
+}
+
+func TestHeadWithPosFiler(t *testing.T) {
+
+	// setting up test file
+	createTestFile()
+
+	content, err := ReadFromHead("./test-file.txt", "HTTP", "", 100, nil)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	lines := strings.Split(content, "\n")
+	numLines := len(lines) - 1
+
+	if numLines != 2 {
+		t.Errorf("Number of lines = %d, Expected 1", numLines)
+	}
+
+	if !strings.Contains(lines[0], "HTTP") {
+		t.Errorf("Expected HTTP, however not found")
+	}
+
+	// removing test file
 	removeTestFile()
 }
