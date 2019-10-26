f7d3eb6c7bf45b2fd3a42b9ba4d28ef949396252
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-10-26 15:01:43 +0530

    Update test for ACLs

--- core/util/checkAcls_test.go
+++ core/util/checkAcls_test.go
@@ -10,7 +10,7 @@ func TestAclsAllowAll(t *testing.T) {
 	// Creating test config
 	testConfig := models.ConfigModel {
 		AllowAll: true,
-		BlockFor: []string{"/blocktest, /blocktest2/subfolder"},
+		BlockFor: []string{"/blocktest", "/block2/subfolder"},
 	}
 
 	// Create test path
@@ -20,5 +20,29 @@ func TestAclsAllowAll(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("Expected nil, however got error")
+	}
+
+	testPath = "/blocktest/test.log"
+
+	err = CheckAcls(testPath, &testConfig)
+
+	if err == nil {
+		t.Errorf("Expected error, however found nil")
+	}
+
+	testPath = "/block/test.log"
+
+	err = CheckAcls(testPath, &testConfig)
+
+	if err != nil {
+		t.Errorf("Expected nil, however found error")
+	}
+
+	testPath = "/block2/subfolder/test.log"
+
+	err = CheckAcls(testPath, &testConfig)
+
+	if err == nil {
+		t.Errorf("Expected Error, however found nil")
 	}
 }
