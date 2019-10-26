68175cda8810b2fe5bffad1416a1338e73cb9bdb
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-10-26 12:47:41 +0530

    Add test for negative regex

--- core/util/util_test.go
+++ core/util/util_test.go
@@ -23,8 +23,31 @@ func TestUtilPosRegex(t *testing.T) {
 	passed = CheckPattern(testString, "^Testing-[0-9]+.*$", "")
 
 	if passed {
-		t.Errorf("Expected talse, however got true")
+		t.Errorf("Expected false, however got true")
 	}
 
 	passed = CheckPattern(testString, "absent", "")
+}
+
+func TestUtilNegRegex(t *testing.T) {
+
+	// Creating test string
+	testString := "This is a test string for positive regex match. Testing-123"
+	passed := CheckPattern(testString, "", "regex")
+
+	if passed {
+		t.Errorf("Expected false, however got true")
+	}
+
+	passed = CheckPattern(testString, "", "Testing-[1-3]+")
+
+	if passed {
+		t.Errorf("Expected false, however got true")
+	}
+
+	passed = CheckPattern(testString, "", "absent")
+
+	if !passed {
+		t.Errorf("Expected true, however got false")
+	}
 }
