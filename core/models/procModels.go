57ae977b51d802e7545b9b578c7919e130c7a72d
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2020-01-11 19:14:30 +0530

    Add model to hold detailed description

--- core/models/procModels.go
+++ core/models/procModels.go
@@ -1,7 +1,20 @@
 package models
 
-type ShortProcDescription struct {
+type ProcDescriptionShort struct {
 	Pid int32
 	Name string
 	CmdLine string
+}
+
+type ProcDescriptionLong struct {
+	Pid int32
+	Ppid int32
+	Name string
+	CmdLine string
+	ExePath string
+	Cwd string
+	Status string
+	Uids []int32
+	Gids []int32
+	Nice []int32
 }
