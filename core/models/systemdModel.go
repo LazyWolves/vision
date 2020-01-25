f03a6dd94a80837f77a8ceb712b291a1af81a347
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2020-01-25 17:37:34 +0530

    Add model for systemd response

--- core/models/systemdModel.go
+++ core/models/systemdModel.go
@@ -4,3 +4,10 @@ type SystemdHolder struct {
 	ServiceName string
 	ServiceState string
 }
+
+type SystemdResponseHolder struct {
+	Services []SystemdHolder
+	NumServices int
+	Timestamp int64
+	TimestampUTC string
+}
