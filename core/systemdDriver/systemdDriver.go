30c87e9521d7ea2d1d146a035654834de1964e33
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2020-01-29 00:21:53 +0530

    Add systemd start stop driver functions

--- core/systemdDriver/systemdDriver.go
+++ core/systemdDriver/systemdDriver.go
@@ -9,3 +9,13 @@ func ListSystemdServices(filterBy []string) (*[]models.SystemdHolder, error) {
 
 	return systemdHandler.ListSystemdServices(filterBy)
 }
+
+func StartSystemdService(target string) (string, error) {
+
+	return systemdHandler.StartSystemdService(target)
+}
+
+func StopSystemdService(target string) (string, error) {
+
+	return systemdHandler.StopSystemdService(target)
+}
